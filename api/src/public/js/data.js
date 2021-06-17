9ad412b5706344dcf84552aced249597844ebbcf
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2021-06-17 14:18:08 +0200

    CSS + data

--- api/src/public/js/data.js
+++ api/src/public/js/data.js
@@ -1,20 +1,12 @@
-import * as CTB64 from "./convertTob64.js";
-
 allprojects()
 
-// async function allUsers() {
-//     await fetch('http://localhost:3000/users/get-all', {
-//         method: 'GET',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         }
-//     }).then(res => {
-//         res.json().then(parsedRes => {
-//             printAllUsers(parsedRes)
-//         })
-//     })
-// }
+$("#search").on("keyup", function () {
+    let valueText = $("input").val();
+
+    if (valueText !== '') {
+        searchProject(valueText)
+    }
+});
 
 async function allprojects() {
     await fetch('/final-work/get-all', {
@@ -32,27 +24,19 @@ async function allprojects() {
     })
 }
 
-// function printAllUsers(allData) {
-//     for (const data in allData) {
-//         $(".item-count").text("Total users: " + data)
-//     }
-//     for (const data of allData) {
-//         $(".table").append(`
-//         <div class="table-tr">
-//             <figure class="table-td">
-//                 <img src="" alt="project-logo">
-//             </figure>
-//             <article class="table-td">
-//                 <p class="bold">${data.name}</p>
-//             </article>
-//             <section class="table-td">
-//                 <p>0 <span>views</span></p>
-//                 <p>0 <span>likes</span></p>
-//             </section>
-//         </div>`);
-//     }
-// }
-
+async function searchProject(query) {
+    await fetch('/final-work/' + query, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(res => {
+        res.json().then(parsedRes => {
+            console.log(parsedRes)
+        })
+    })
+}
 
 function printProjects(allData) {
     for (const data in allData) {
