ed6ea27f829a07e4bc112315f248be5800d10a59
Ayoub <ayoub.deneubourg@student.ehb.be>
2021-06-21 18:53:25 +0200

    favorite & votes route : adding username name, all rights admin

--- api/src/routes/admin/my-favorites.js
+++ api/src/routes/admin/my-favorites.js
@@ -7,7 +7,16 @@ router.post("/", async (req, res) => {
 
   try {
     const allVotedProjects = await pool.query(
-      `SELECT name, project_id, cluster FROM favorites INNER JOIN projects ON favorites.project_id = projects.projectid AND favorites.user_id = ${docentId}`
+      `SELECT
+      projects.name,
+      projects.projectid,
+      projects.cluster,
+      users.username
+  FROM
+  favorites
+      INNER JOIN projects ON  favorites.project_id = projects.projectid
+      AND favorites.user_id = ${docentId}
+      INNER JOIN users ON users.userid = projects.user_id `
     );
 
     res.send(allVotedProjects.rows);
