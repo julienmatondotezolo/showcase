d9491906eb92fafb74779450b597aec522dc0a08
Youssef Sefiani <youssef.sefiani@student.ehb.be>
2021-06-22 19:59:28 +0200

    added nomination logic

--- api/src/routes/admin/nominate.js
+++ api/src/routes/admin/nominate.js
@@ -2,17 +2,79 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../../db/db");
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const sortParam = req.params.sort;
-    console.log(sortParam);
-    let sort = sortParam ? sortParam : "ASC";
-
-    const allFinalWorksSQL = await pool.query(`SELECT * from projects`);
-    res.status(200).json(allFinalWorksSQL.rows);
+    checkNominationList();
   } catch (err) {
-    console.error(err.message);
     res.sendCustomStatus(500);
+  }
+
+  async function checkNominationList() {
+    let { id } = req.body;
+    let docentId = req.user.userid;
+
+    const allDocentNominationList = await pool.query(
+      `SELECT 
+      nominations.id, 
+      nominations.project_id, 
+      projects."cluster", 
+      projects."name"  
+      FROM 
+      nominations 
+      INNER JOIN 
+      projects 
+      ON nominations.project_id = 
+      projects.projectid 
+      AND nominations.user_id = ${docentId}`
+    );
+
+    const votedProject = await pool.query(
+      `SELECT * FROM projects WHERE projectid = ${id}`
+    );
+
+    if (!allDocentNominationList.rows.length) {
+      giveCorrectGradeDependingOnPosition(1, 3);
+    } else {
+      if (checkIfDifferentCluster(allDocentNominationList.rows, votedProject.rows[0].cluster)) {
+        giveCorrectGradeDependingOnPosition(allDocentNominationList.rows.length + 1, 3);
+      }
+    }
+  };
+
+  async function giveCorrectGradeDependingOnPosition(rankPosition, maxPosition) {
+
+    const availableGrades = [5, 3, 1];
+    let gradeToGive = availableGrades[rankPosition - 1];
+
+    let { id } = req.body;
+    let docentId = req.user.userid;
+    let values = [id, docentId, gradeToGive];
+
+    if (rankPosition < 1 || rankPosition > maxPosition) {
+      res.sendCustomStatus(400, `Cannot vote for more than ${maxPosition} projects`);
+      return;
+    }
+
+    const insertGrade = await pool.query(
+      "INSERT INTO nominations(project_id, user_id, points) VALUES($1, $2, $3) RETURNING *",
+      values
+    );
+    res.sendCustomStatus(200, "Project successfully added!");
+    return gradeToGive;
+  }
+
+  function checkIfDifferentCluster(projectsAlreadyVoted, projectToVoteCluster) {
+    let canVote = true;
+    projectsAlreadyVoted.forEach(project => {
+      if (project.cluster === projectToVoteCluster) {
+        res.sendCustomStatus(400, "cannot vote for 2 projects of same cluster");
+        canVote = false;
+        return false;
+      }
+    })
+    if (canVote) {
+      return true;
+    }
   }
 });
 module.exports = router;
