183d0a48a15c2a84699fa0afa096a6e8739c009e
Ayoub <ayoub.deneubourg@student.ehb.be>
2021-06-24 13:27:09 +0200

    my-nominations

--- api/src/routes/admin/get-nominations.js
+++ api/src/routes/admin/get-nominations.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
     const sortParam = req.params.sort;
     console.log(sortParam);
 
-    const allFinalWorksSQL = await pool.query(
+    /*     const allFinalWorksSQL = await pool.query(
       `SELECT projects.name, projects.description, projects.cluster,  projects.images, projects.url from projects`
     );
 
@@ -32,7 +32,23 @@ router.get("/", async (req, res) => {
     object["mobile"][0].isWinner = true;
     object["digital-making"][0].isWinner = true;
 
-    res.status(200).json(object);
+    res.status(200).json(object); */
+
+    const allVotedProjects = await pool.query(
+      `SELECT votes.id, projects.name,projects.cluster, projects.images, users.username FROM votes INNER JOIN projects ON votes.project_id = projects.projectid INNER JOIN users ON votes.user_id = users.userid ORDER BY project_id ASC`
+    );
+
+    let object = {};
+
+    allVotedProjects.rows.forEach((item) => {
+      object[item.name] = object[item.name] ? object[item.name] + 1 : 1;
+    });
+
+    allVotedProjects.rows.forEach((item) => {
+      item.totalVotes = object[item["name"]];
+    });
+
+    res.send(allVotedProjects.rows);
   } catch (err) {
     console.error(err.message);
     res.sendCustomStatus(500);
