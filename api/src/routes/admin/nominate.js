1c31bbafeae41950600c79b0d5ac64e07750cc38
Youssef Sefiani <youssef.sefiani@student.ehb.be>
2021-06-24 14:22:35 +0200

    fixed nomination positions

--- api/src/routes/admin/nominate.js
+++ api/src/routes/admin/nominate.js
@@ -5,14 +5,15 @@ const pool = require("../../db/db");
 router.post("/", async (req, res) => {
   try {
     checkNominationList();
+
   } catch (err) {
     res.sendCustomStatus(500);
   }
 
   async function checkNominationList() {
-    let { id } = req.body;
+    let { id, position } = req.body;
     let docentId = req.user.userid;
-
+    console.log(req.body);
     const allDocentNominationList = await pool.query(
       `SELECT 
       nominations.id, 
@@ -32,46 +33,81 @@ router.post("/", async (req, res) => {
       `SELECT * FROM projects WHERE projectid = ${id}`
     );
 
+    const nominatedProjects = await pool.query(
+      `SELECT * FROM nominations WHERE user_id = ${docentId}`
+    );
 
+    console.log("here!!!!", nominatedProjects.rows);
 
-    console.log("here id", id);
-    console.log("here", votedProject.rows[0]);
+    let maxNominations = 3;
+
+    function checkIfMoreNominationsThanPossible() {
+      if (allDocentNominationList.rows.length === maxNominations) {
+        res.sendCustomStatus(400, `Cannot have more than ${allDocentNominationList.rows.length} nominations`);
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    function positionToPoints(position) {
+
+      if (position === 1) {
+        return 5;
+      } else if (position === 2) {
+        return 3;
+      } else {
+        return 1;
+      }
+
+    }
+
+    function checkIfPositionAlreadyTaken(position) {
+
+      let canVote = true;
+      nominatedProjects.rows.forEach(project => {
+        if (project.points === positionToPoints(position)) {
+          res.sendCustomStatus(400, "Already voted for this position");
+          canVote = false;
+          return false;
+        }
+      })
+      return canVote;
+    }
 
     if (!allDocentNominationList.rows.length) {
-      giveCorrectGradeDependingOnPosition(1, 3);
+      giveCorrectGradeDependingOnPosition(position);
     } else {
       if (
         checkIfDifferentCluster(
           allDocentNominationList.rows,
           votedProject.rows[0].cluster
-        )
+        ) && checkIfMoreNominationsThanPossible()
+        && checkIfPositionAlreadyTaken(position)
       ) {
         giveCorrectGradeDependingOnPosition(
-          allDocentNominationList.rows.length + 1,
-          3
+          position
         );
       }
     }
   }
 
   async function giveCorrectGradeDependingOnPosition(
-    rankPosition,
-    maxPosition
+    rankPosition
   ) {
     const availableGrades = [5, 3, 1];
     let gradeToGive = availableGrades[rankPosition - 1];
-
     let { id } = req.body;
     let docentId = req.user.userid;
     let values = [id, docentId, gradeToGive];
-
-    if (rankPosition < 1 || rankPosition > maxPosition) {
-      res.sendCustomStatus(
-        400,
-        `Cannot vote for more than ${maxPosition} projects`
-      );
-      return;
-    }
+    /* 
+        if (rankPosition < 1 || rankPosition > maxPosition) {
+          res.sendCustomStatus(
+            400,
+            `Cannot nominate more than ${maxPosition} projects`
+          );
+          return;
+        } */
 
     const insertGrade = await pool.query(
       "INSERT INTO nominations(project_id, user_id, points) VALUES($1, $2, $3) RETURNING *",
@@ -80,7 +116,7 @@ router.post("/", async (req, res) => {
     const nominatedProject = await pool.query(
       `SELECT name FROM projects where projectid = ${id}`
     );
-
+    console.log("entered here my friend");
     res.sendCustomStatus(
       200,
       `Project ${nominatedProject.rows[0].name} successfully nominated!`
@@ -92,15 +128,14 @@ router.post("/", async (req, res) => {
   function checkIfDifferentCluster(projectsAlreadyVoted, projectToVoteCluster) {
     let canVote = true;
     projectsAlreadyVoted.forEach((project) => {
+
       if (project.cluster === projectToVoteCluster) {
         res.sendCustomStatus(400, "cannot vote for 2 projects of same cluster");
         canVote = false;
         return false;
       }
     });
-    if (canVote) {
-      return true;
-    }
+    return canVote;
   }
 });
 module.exports = router;
