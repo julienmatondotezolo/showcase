dc21c4ed8cf68c979eb4de0b7854c05e70f52b37
Ayoub <ayoub.deneubourg@student.ehb.be>
2021-06-25 16:08:08 +0200

    routes superprijs

--- api/src/routes/admin/set-superprijs.js
+++ api/src/routes/admin/set-superprijs.js
@@ -7,7 +7,7 @@ router.post("/", async (req, res) => {
     let { id } = req.body;
 
     const allWinners = await pool.query(
-      `SELECT projects.name, projects.cluster, projects.winner, projects.projectid from projects where winner = TRUE`
+      `SELECT projects.name, projects.cluster, projects.winner, projects.projectid from projects where superprijs = TRUE`
     );
 
     if (!allWinners.rows.length) {
@@ -15,12 +15,20 @@ router.post("/", async (req, res) => {
         "UPDATE projects SET superprijs = $1 WHERE projectid = $2",
         [true, id]
       );
-      res.sendCustomStatus(400, "Already a winner for this cluster!");
+      res.sendCustomStatus(200, `The project is sucessfully added.`);
     } else {
-      const removeWinner = await pool.query(
-        "UPDATE projects SET superprijs = $1 WHERE projectid = $2",
-        [false, id]
-      );
+      if (allWinners.rows[0].projectid == id) {
+        const removeWinner = await pool.query(
+          "UPDATE projects SET superprijs = $1 WHERE projectid = $2",
+          [false, id]
+        );
+        res.sendCustomStatus(
+          200,
+          `The project ${allWinners.rows[0].name} is not more Superprijs.`
+        );
+      } else {
+        res.sendCustomStatus(403, "Already a winner for the superprijs!");
+      }
     }
   } catch (err) {
     console.log(err);
